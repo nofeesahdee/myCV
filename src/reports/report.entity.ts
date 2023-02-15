eb13568eede1b3a1f23fe5b58d657ba801fb01df
Dolapo <nofeesahdee@gmail.com>
2023-02-15 14:38:27 +0100

    connect to database

---
+++ src/reports/report.entity.ts
@@ -0,0 +1,13 @@
+import { Entity, Column, PrimaryGeneratedColumn  } from "typeorm";
+
+@Entity()
+export class Report {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    email: string;
+
+    @Column()
+    password: string;
+}
