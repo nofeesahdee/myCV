41c7e8b76c6509bb287f66b0f58e0755c84a8a19
Dolapo <nofeesahdee@gmail.com>
2023-02-23 12:09:02 +0100

    production

--- ormconfig.js
+++ ormconfig.js
@@ -1,5 +1,9 @@
 const dbConfig = {
     synchronize: false,
+    migrations: ['migrations/*.js'],
+    cli:{
+        migrationsDir:'migrations'
+    },
 }
 
 switch(process.env.NODE_ENV){
@@ -14,10 +18,20 @@ switch(process.env.NODE_ENV){
         Object.assign(dbConfig, {
             type: 'sqlite',
             database: 'test.sqlite',
-            entities: ['**/*.entity.ts']
+            entities: ['**/*.entity.ts'],
+            migrationsRun: true
         });
         break;
     case 'production':
+        Object.assign(dbConfig, {
+            type: 'postgres',
+            url: process.env.DATABASE_URL,
+            migrationsRun: true,
+            entities: ['**/*.entity.js'],
+            ssl: {
+                rejectUnauthorized: false
+            }
+        });
         break;
     default:
         throw new Error('unknown environment')
